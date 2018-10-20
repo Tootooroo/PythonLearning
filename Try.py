5f25f921dece5516fae0d6642cf113a91960a9ed
Ayden Lin <aydenlin@foxmail.com>
2018-10-20 09:35:36 +0800

    Draft created

--- Try.py
+++ Try.py
@@ -5,8 +5,8 @@ class Section:
 
     def __init__(self, secA_, secB_, secC_):
         self.secA = secA_
-        self.secB_ = secB_
-        self.secC_ = secC_
+        self.secB = secB_
+        self.secC = secC_
 
     def secAVal(self):
         return self.secA
@@ -29,39 +29,54 @@ class Path:
     def pathAppend(self, path_):
         self.path.append(path_)
 
+    def getPath(self):
+        return self.path
+
+    def setPath(self, path_):
+        self.path = path_
+
 def optimalPath(pathPair, section): 
     pathA = pathPair[0]
     pathB = pathPair[1]
     
-    totalA = ''
-    for v in pathA:
+    totalA = 0
+    for v in pathA.getPath():
         totalA += v[1] 
 
-    totalB = ''
-    for v in pathB:
+    totalB = 0
+    for v in pathB.getPath():
         totalB += v[1]
 
-    toAFromA = ''
+    toAFromA = 0
     toAFromA = totalA + section.secAVal()
-    toAFromB = ''
+    toAFromB = 0
     toAFromB = totalB + section.secBVal() + section.secCVal()
-    toBFromA = ''
+    toBFromA = 0
     toBFromA = totalA + section.secAVal() + section.secCVal()
-    toBFromB = ''
+    toBFromB = 0
     toBFromB = totalB + section.secBVal()
+    
+    newPathToA = list()
+    newPathToB = list()
 
-    if toAFromA < toAFromB:
-        pathA.pathAppend(('A', section.secAVal()))
+    if toAFromA <= toAFromB:
+        newPathToA += pathA.getPath()
+        newPathToA.append(('A', section.secAVal()))
     else:
-        pathA.pathAppend(('B', section.secBVal()))
-        pathA.pathAppend(('C', section.secCVal()))
+        newPathToA += pathB.getPath()
+        newPathToA.append(('B', section.secBVal()))
+        newPathToA.append(('C', section.secCVal()))
 
-    if toBFromA < toBFromB:
-        pathB.pathAppend(('A', section.secAVal()))
-        pathB.pathAppend(('C', section.secCVal()))
+    if toBFromA <= toBFromB:
+        newPathToB += pathA.getPath()
+        newPathToB.append(('A', section.secAVal()))
+        newPathToB.append(('C', section.secCVal()))
     else:
-        pathB.pathAppend(('B', section.secBVal()))
-        
+        newPathToB += pathB.getPath()
+        newPathToB.append(('B', section.secBVal()))
+    
+    pathA.setPath(newPathToA)
+    pathB.setPath(newPathToB)
     return (pathA, pathB)
 
 def main():
@@ -73,4 +88,7 @@ def main():
     ]
     for sec in secArray:
         optimalPath((pathA, pathB), sec)
+    pathA.pathPrint();
+    pathB.pathPrint();
 
+main()
